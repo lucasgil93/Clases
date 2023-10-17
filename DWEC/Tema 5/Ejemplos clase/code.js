64cee97305082cb921b8da0e59c6fb73202d371a
lucasgil93 <lucasgildominguez@gmail.com>
2023-10-17 19:37:53 +0200

    19:37 Commit 17-oct-23

--- DWEC/Tema 5/Ejemplos clase/code.js
+++ DWEC/Tema 5/Ejemplos clase/code.js
@@ -7,9 +7,9 @@ let objetovacio1 = new Object;
 
 
 //Objeto con cosas
-let alumno ={
+let alumno = {
 
-    nombre:"Javier",
+    nombre: "Javier",
     edad: 42,
 }
 
@@ -17,31 +17,52 @@ let alumno ={
 
 console.log(`Nombre: ${alumno.nombre} Edad: ${alumno.edad}`);
 
-alumno ={
+alumno = {
 
-    nombre:"Javier",
+    nombre: "Javier",
     edad: 42,
-    saludo: function(){
+    saludo: function () {
         return `Hola soy ${this.nombre} y tengo ${this.edad} años`;
     },
-    mostrar(){
+    mostrar() {
         return `Nombre: ${this.nombre} Edad: ${this.edad}`;
     },
-    saludoFlecha: ()=>{
-        return `Hola soy ${this.nombre} y tengo ${this.edad} años`; //el this se desvincula. y apareceria undefined.
+    saludoFlecha: () => {
+        return `Hola soy ${this.nombre} y tengo ${this.edad} años`; //el this se desvincula. y apareceria undefined y no se deben usar
     }
 }
 
+
+
 console.log(alumno.saludo());
 console.log(alumno.mostrar());
+console.log(alumno.saludoFlecha()); 
 
 alumno.edad = 43;
 
-alumno.getEdad = function(){
+alumno.getEdad = function () {
 
     return this.edad;
 }
 
 console.log("Edad del alumno: " + alumno.getEdad());
 
+function hola(){
 
+    return this.nombre;
+}
+
+let alumno2={
+    nombre:"Lucas",
+    hola:hola
+
+} 
+
+let alumno3={
+    nombre:"Paco",
+    hola: hola
+}
+
+
+console.log(alumno2.hola());
+console.log(alumno3.hola());
