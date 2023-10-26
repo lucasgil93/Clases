cc3c6f6538598f5b03ed86ed2024958a090f8922
lucasgil93 <lucasgildominguez@gmail.com>
2023-10-26 13:43:04 +0200

    hasta aqui funciona

--- DWEC/Practica Cuestionario/code.js
+++ DWEC/Practica Cuestionario/code.js
@@ -12,10 +12,16 @@ let arrayPreguntas = [];
 let divPreguntas = document.querySelector("#divPreguntas");
 let divErrores = document.querySelector("#divErrores");
 
+let cont = -1;
 
 btnCrear.addEventListener("click", añadirPregunta);
 
 function añadirPregunta() {
+
+
+    // añadir contador en -1 para que cuando se cree el primer obejto sea la posicion 0 del array y se pueda llamar desde los metodos descartar y recueprar
+
+ 
 
     divErrores.innerHTML = "";
 
@@ -55,7 +61,9 @@ function añadirPregunta() {
         sol += `<p>${arrayTextos[3]}</p><br>`;
         sol += `<p>${arrayTextos[4]}</p><br>`;
 
+
         const pregunta1 = new Pregunta(arrayTextos[0], arrayTextos[1], arrayTextos[2], arrayTextos[3], arrayTextos[4]);
+        cont ++;
         arrayPreguntas.push(pregunta1);
 
     }
@@ -65,20 +73,16 @@ function añadirPregunta() {
     //boton descartar (no preguntes como esta hecho el boton jaja);
 
     const btnDescartar = document.createElement('button');
-    btnDescartar.textContent = 'Descartar Pregunta';
+    btnDescartar.textContent = 'Descartar Pregunta'+cont;
     document.body.appendChild(btnDescartar);
-
-
     section.appendChild(btnDescartar);
 
 
 
     // boton recuperar (funciona de momento)
     const btnRecuperar = document.createElement('button');
-    btnRecuperar.textContent = 'Recuperar Pregunta';
+    btnRecuperar.textContent = 'Recuperar Pregunta'+cont;
     document.body.appendChild(btnRecuperar);
-
-
     section.appendChild(btnRecuperar);
 
 
@@ -88,8 +92,8 @@ function añadirPregunta() {
     divPreguntas.innerHTML += sol;
 
     divPreguntas.appendChild(section);
-    btnRecuperar.addEventListener('click', recuperar);
-    btnDescartar.addEventListener('click', descartar);
+    btnRecuperar.addEventListener('click', function (){recuperar(cont)});
+    btnDescartar.addEventListener('click', function (){descartar(cont)});
 
 
 
@@ -108,14 +112,14 @@ function guardarPreguntas() {
 
 
 
-function descartar() {
+function descartar(cont) {
 
-    alert("descarta");
+    alert(cont);
 }
 
 
 
-function recuperar() {
+function recuperar(cont) {
 
-    alert("recupera");
+    alert(cont);
 }
