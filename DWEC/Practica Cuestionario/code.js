62ef9e39fbf19b848047a744e037460c5a30136b
lucasgil93 <lucasgildominguez@gmail.com>
2023-10-26 10:31:24 +0200

    Proyecto Moodle

---
+++ DWEC/Practica Cuestionario/code.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let txtPregunta = document.querySelector("#txtPregunta");
+let txtCorrecta = document.querySelector("#txtCorrecta");
+let txt
---
+++ DWEC/Practica Cuestionario/objetos.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class Pregunta {
+
+    constructor(pregunta, rCorrecta, rI1, rI2, rI3){
+
+        this.pregunta = pregunta;
+        this.rCorrecta = rCorrecta;
+        this.rI1 = rI1;
+        this.rI2 = rI2;
+        this.rI3 = rI3;
+    }
+
+    
+
+}
