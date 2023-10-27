6759afba2f0e62da17e98a5deb217ff265ce827c
lucasgil93 <lucasgildominguez@gmail.com>
2023-10-27 20:05:26 +0200

    27 yes

--- DIW/Proyecto/code.js
+++ DIW/Proyecto/code.js
@@ -1,13 +1,15 @@
-const btnMode = document.getElementById("btnMode");
+"use strict";
 
-btnMode.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark-mode");
+  const btnMode = document.getElementById("btnMode");
 
-  if (document.documentElement.classList.contains("dark-mode")) {
-    // Dark mode
+  // Function to set dark mode CSS variables
+  const setDarkMode = () => {
+    document.documentElement.classList.add("dark-mode");
+
+    // Dark mode variables
     document.documentElement.style.setProperty("--darkestgreen", "#562C2C");
     document.documentElement.style.setProperty("--darkergreen", "#A4402D");
-    document.documentElement.style.setProperty("--green-logo", "#CB4A2D");
+    document.documentElement.style.setProperty("--green-logo", "#AE432D");
     document.documentElement.style.setProperty("--caribbean", "#F2542D");
     document.documentElement.style.setProperty("--airblue", "#F37751");
     document.documentElement.style.setProperty("--greyblue", "#F49A74");
@@ -17,9 +19,17 @@ btnMode.addEventListener("click", () => {
     document.documentElement.style.setProperty("--lightestperi", "#127475");
     document.documentElement.style.setProperty("--lavander", "#C7CAE1");
     document.documentElement.style.setProperty("--lightlavender", "#CCCFE4");
-    document.documentElement.style.setProperty("--beige","#f5f5dc");
-   } else {
-    // Light mode
+    document.documentElement.style.setProperty("--beige", "#f5f5dc");
+
+    // Save dark mode state to local storage
+    localStorage.setItem("darkMode", "true");
+  };
+
+  // Function to set light mode CSS variables
+  const setLightMode = () => {
+    document.documentElement.classList.remove("dark-mode");
+
+    // Light mode variables
     document.documentElement.style.setProperty("--darkestgreen", "#003535");
     document.documentElement.style.setProperty("--darkergreen", "#004D4D");
     document.documentElement.style.setProperty("--green-logo", "#005555");
@@ -32,6 +42,24 @@ btnMode.addEventListener("click", () => {
     document.documentElement.style.setProperty("--lightestperi", "#C1C5DE");
     document.documentElement.style.setProperty("--lavander", "#C7CAE1");
     document.documentElement.style.setProperty("--lightlavender", "#CCCFE4");
-    document.documentElement.style.setProperty("--beige","#f5f5dc");
+    document.documentElement.style.setProperty("--beige", "#f5f5dc");
+
+    // Save light mode state to local storage
+    localStorage.setItem("darkMode", "false");
+  };
+
+  btnMode.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("dark-mode")) {
+      setLightMode();
+    } else {
+      setDarkMode();
+    }
+  });
+
+  // Check local storage for dark mode state
+  const isDarkMode = localStorage.getItem("darkMode");
+  if (isDarkMode === "true") {
+    setDarkMode();
+  } else {
+    setLightMode();
   }
-});
