a253ec009acaed632506ad08eb02c904f34a6c3e
lucasgil93 <lucasgildominguez@gmail.com>
2023-10-28 22:52:38 +0200

    28-oct

--- DIW/Proyecto/code.js
+++ DIW/Proyecto/code.js
@@ -8,7 +8,6 @@
 
 
     document.documentElement.style.setProperty("--darkestgreen", "#562C2C");
-    document.documentElement.style.setProperty("--darkergreen", "#A4402D");
     document.documentElement.style.setProperty("--green-logo", "#AE432D");
     document.documentElement.style.setProperty("--caribbean", "#F2542D");
     document.documentElement.style.setProperty("--airblue", "#F37751");
@@ -16,7 +15,6 @@
     document.documentElement.style.setProperty("--periwinkle", "#F5BD98");
     document.documentElement.style.setProperty("--lightperi", "#F5DFBB");
     document.documentElement.style.setProperty("--lavander", "#84AA98");
-    document.documentElement.style.setProperty("--lightlavander", "#127475");
     document.documentElement.style.setProperty("--beige", "#f5f5dc");
 
 
@@ -27,17 +25,12 @@
     document.documentElement.classList.remove("dark-mode");
 
 
-    document.documentElement.style.setProperty("--darkestgreen", "#003535");
-    document.documentElement.style.setProperty("--darkergreen", "#004D4D");
-    document.documentElement.style.setProperty("--green-logo", "#005555");
-    document.documentElement.style.setProperty("--caribbean", "#2C6D75");
-    document.documentElement.style.setProperty("--airblue", "#578494");
-    document.documentElement.style.setProperty("--greyblue", "#829BB4");
-    document.documentElement.style.setProperty("--periwinkle", "#ADB2D3");
-    document.documentElement.style.setProperty("--lightperi", "#B4B9D7");
-    document.documentElement.style.setProperty("--lavander", "#C7CAE1");
-    document.documentElement.style.setProperty("--lightlavender", "#CCCFE4");
-    document.documentElement.style.setProperty("--beige", "#f5f5dc");
+    document.documentElement.style.setProperty("--darkestgreen", "#453A49"); 
+    document.documentElement.style.setProperty("--green-logo", "#005555"); //DarkSlateGray
+    document.documentElement.style.setProperty("--caribbean", "#6D3B47"); //wine
+    document.documentElement.style.setProperty("--airblue", "#f2542d"); //cinnabar
+    document.documentElement.style.setProperty("--lightperi", "#f2542d"); //cinabar
+    document.documentElement.style.setProperty("--beige", "#F9DBBD"); //lightorange
 
     localStorage.setItem("darkMode", "false");
   };
