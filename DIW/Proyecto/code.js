97dbea3bacc08afd725f53d6ddf9a2983231247a
lucasgil93 <lucasgildominguez@gmail.com>
2023-10-30 22:34:53 +0100

    commit casa final cv

--- DIW/Proyecto/code.js
+++ DIW/Proyecto/code.js
@@ -7,12 +7,12 @@
     document.documentElement.classList.add("dark-mode");
 
 
-    document.documentElement.style.setProperty("--darkest", "#EBFFFF");
-    document.documentElement.style.setProperty("--dark", "#ADFFFF");
-    document.documentElement.style.setProperty("--medium", "#00CCCC"); //wine
-    document.documentElement.style.setProperty("--light", "#6D3B47"); //cinnabar
-    document.documentElement.style.setProperty("--lighter", "#005555"); //cinabar
-    document.documentElement.style.setProperty("--lightest", "#003535"); //lightorange
+    document.documentElement.style.setProperty("--darkest", "#EBFFFF"); //lighterblue
+    document.documentElement.style.setProperty("--dark", "#ADFFFF"); //lightblue
+    document.documentElement.style.setProperty("--medium", "#00CCCC"); 
+    document.documentElement.style.setProperty("--light", "#6D3B47"); 
+    document.documentElement.style.setProperty("--lighter", "#005555"); 
+    document.documentElement.style.setProperty("--lightest", "#003535"); 
     document.documentElement.style.setProperty("--accent", "#F9DBBD"); 
 
 
@@ -27,8 +27,8 @@
     document.documentElement.style.setProperty("--dark", "#005555");
     document.documentElement.style.setProperty("--medium", "#409CA9"); 
     document.documentElement.style.setProperty("--light", "#D1CCBB"); 
-    document.documentElement.style.setProperty("--lighter", "#F69875"); 
-    document.documentElement.style.setProperty("--lightest", "#FAE2CB"); 
+    document.documentElement.style.setProperty("--lighter", "#ADFFFF"); 
+    document.documentElement.style.setProperty("--lightest", "#EBFFFF"); 
     document.documentElement.style.setProperty("--accent", "#F47656"); 
 
     localStorage.setItem("darkMode", "false");
