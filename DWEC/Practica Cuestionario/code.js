cf9862c25d75acc39010d6529e5db4a5206d7716
lucasgil93 <lucasgildominguez@gmail.com>
2023-11-02 21:03:46 +0100

    funciona con el formato del teacher

--- DWEC/Practica Cuestionario/code.js
+++ DWEC/Practica Cuestionario/code.js
@@ -1,192 +1,96 @@
 "use strict";
 
-
-
 let btnCrear = document.querySelector("#btnCrear");
 let btnGenerarArchivo = document.querySelector("#btnGenerarArchivo");
 let btnBorrar = document.querySelector("#btnBorrar");
 let btnGuardar = document.querySelector("#btnGuardar");
 
-let arrayPreguntas = [];
+let cuestionario = new Cuestionario(); // Objeto cuestionario para almacenar las preguntas
 
 let divPreguntas = document.querySelector("#divPreguntas");
 let divErrores = document.querySelector("#divErrores");
 
-let cont = -1; //Este contador habria que meterlo en el array de textos o en el propio objeto pregunta para que los metodos getPregunta y descartarPregunta puedan acceder a el y saber cual es la pregunta a modificar o borrar!!
-
 btnCrear.addEventListener("click", añadirPregunta);
 
 function añadirPregunta() {
-
-    divEnlace.innerHTML="";
-    // añadir contador en -1 para que cuando se cree el primer obejto sea la posicion 0 del array y se pueda llamar desde los metodos descartarPregunta y recueprar
     divErrores.innerHTML = "";
+    const txtPregunta = document.querySelector("#txtPregunta").value;
+    const txtCorrecta = document.querySelector("#txtCorrecta").value;
+    const txtIncorrecta1 = document.querySelector("#txtIncorrecta1").value;
+    const txtIncorrecta2 = document.querySelector("#txtIncorrecta2").value;
+    const txtIncorrecta3 = document.querySelector("#txtIncorrecta3").value;
 
-    let arrayTextos = [];
+    if (txtPregunta === "" || txtCorrecta === "" || txtIncorrecta1 === "" || txtIncorrecta2 === "" || txtIncorrecta3 === "") {
+        divErrores.innerHTML = "Hay campos vacíos.";
+    } else {
+        const pregunta = new Pregunta(txtPregunta, txtCorrecta, txtIncorrecta1, txtIncorrecta2, txtIncorrecta3);
+        cuestionario.añadirPregunta(pregunta);
 
-    let sol = `<section>`;
+        actualizarDivPreguntas();
+    }
 
-    let txtPregunta = document.querySelector("#txtPregunta").value;
-    let txtCorrecta = document.querySelector("#txtCorrecta").value;
-    let txtIncorrecta1 = document.querySelector("#txtIncorrecta1").value;
-    let txtIncorrecta2 = document.querySelector("#txtIncorrecta2").value;
-    let txtIncorrecta3 = document.querySelector("#txtIncorrecta3").value;
-
-
+    // Limpiar campos de entrada
     document.getElementById("txtPregunta").value = "";
     document.getElementById("txtCorrecta").value = "";
     document.getElementById("txtIncorrecta1").value = "";
     document.getElementById("txtIncorrecta2").value = "";
     document.getElementById("txtIncorrecta3").value = "";
-
-    if (txtPregunta == "" || txtCorrecta == "" || txtIncorrecta1 == "" || txtIncorrecta2 == "" || txtIncorrecta3 == "") {
-        divErrores.innerHTML = "Hay campos vacios."
-    } else {
-
-        arrayTextos.push(txtPregunta);
-        arrayTextos.push(txtCorrecta);
-        arrayTextos.push(txtIncorrecta1);
-        arrayTextos.push(txtIncorrecta2);
-        arrayTextos.push(txtIncorrecta3);
-
-        for (let index = 0; index < arrayTextos.length; index++) {
-            arrayTextos[index] = arrayTextos[index].replaceAll("#", "\\#");
-            arrayTextos[index] = arrayTextos[index].replaceAll("~", "\\~");
-            arrayTextos[index] = arrayTextos[index].replaceAll("=", "\\=");
-            arrayTextos[index] = arrayTextos[index].replaceAll("{", "\\{");
-            arrayTextos[index] = arrayTextos[index].replaceAll("}", "\\}");
-            arrayTextos[index] = arrayTextos[index].replaceAll(":", "\\:");
-
-        }
-
-        sol += `<h3>${arrayTextos[0]}</h3>`;
-        sol += `<p>${arrayTextos[1]}</p><br>`;
-        sol += `<p>${arrayTextos[2]}</p><br>`;
-        sol += `<p>${arrayTextos[3]}</p><br>`;
-        sol += `<p>${arrayTextos[4]}</p><br>`;
-
-
-        const pregunta1 = new Pregunta(arrayTextos[0], arrayTextos[1], arrayTextos[2], arrayTextos[3], arrayTextos[4]);
-        cont++;
-        arrayPreguntas.push(pregunta1);
-
-
-        sol += `<button type="button" onclick="descartarPregunta(this.id)" id=${cont}>Descartar Pregunta</button>`
-        sol += `<button type="button" onclick="getPregunta(this.id)" id=${cont}>Recuperar Pregunta</button>`
-
-
-        sol += `</section>`
-
-
-        divPreguntas.innerHTML += sol;
-
-
-    }
-
 }
-
 
 btnBorrar.addEventListener("click", borrarPreguntas);
 btnGuardar.addEventListener("click", guardarPreguntas);
 btnGenerarArchivo.addEventListener("click", generaArchivo);
 
-function guardarPreguntas() {
-
-const cuestiones = arrayPreguntas.map((pregunta)=>({
-    pregunta: pregunta.pregunta,
-    rCorrecta: pregunta.rCorrecta,
-    rI1: pregunta.rI1,
-    rI2: pregunta.rI2,
-    rI3: pregunta.rI3,
-}));
-
-const cuestionario1 = new Cuestionario(cuestiones);
-const jsonString = JSON.stringify(cuestionario1);
-localStorage.setItem("cuestionario", jsonString);
-
-
-
-}
-
 function borrarPreguntas() {
-
-    arrayPreguntas = [];
-    divErrores.innerHTML += "Se han borrado las preguntas.";
-    divPreguntas.innerHTML = "";
-    cont = -1;
-    if (arrayPreguntas.length>0){
-        divPreguntas.innerHTML+=`<button type="button" onclick="descartarPregunta(0)" id=0>Descartar Pregunta</button>`;
-           
-    };
+    cuestionario.borrarPreguntas();
+    divErrores.innerHTML = "Se han borrado las preguntas.";
+    actualizarDivPreguntas();
     localStorage.removeItem("cuestionario");
 }
 
-
-function descartarPregunta(x) {
-    arrayPreguntas.splice(x, 1);
-    cont--;
+function actualizarDivPreguntas() {
     divPreguntas.innerHTML = "";
 
-    if (arrayPreguntas.length === 0) {
-        divErrores.innerHTML = "Todavia no hay preguntas creadas.";
+    if (cuestionario.preguntas.length === 0) {
+        divErrores.innerHTML = "Todavía no hay preguntas creadas.";
     } else {
-        arrayPreguntas.forEach((pregunta, index) => {
-            let sol = `<section>`;
-            sol += `<h3>${pregunta.pregunta}</h3>`;
-            sol += `<p>${pregunta.rCorrecta}</p><br>`;
-            sol += `<p>${pregunta.rI1}</p><br>`;
-            sol += `<p>${pregunta.rI2}</p><br>`;
-            sol += `<p>${pregunta.rI3}</p><br>`;
-            sol += `<button type="button" onclick="descartarPregunta(${index})" id=${index}>Descartar Pregunta</button>`;
-            sol += `<button type="button" onclick="getPregunta(${index})" id=${index}>Recuperar Pregunta</button>`;
-            sol += `</section>`;
+        cuestionario.preguntas.forEach((pregunta, index) => {
+            const preguntaHTML = pregunta.toHTMLUl();
+            const sol = `<section><h3>${pregunta.pregunta}</h3>${preguntaHTML}
+                        <button type="button" onclick="descartarPregunta(${index})" id="${index}">Descartar Pregunta</button>
+                        <button type="button" onclick="getPregunta(${index})" id="${index}">Recuperar Pregunta</button></section>`;
             divPreguntas.innerHTML += sol;
         });
     }
 }
 
-function getPregunta(x) {
+function descartarPregunta(id) {
+    cuestionario.descartarPregunta(id);
+    actualizarDivPreguntas();
+}
 
-    let pregunta1 = arrayPreguntas[x];
+function getPregunta(id) {
+    const pregunta = cuestionario.getPregunta(id);
+    document.getElementById("txtPregunta").value = pregunta.pregunta;
+    document.getElementById("txtCorrecta").value = pregunta.rCorrecta;
+    document.getElementById("txtIncorrecta1").value = pregunta.rI1;
+    document.getElementById("txtIncorrecta2").value = pregunta.rI2;
+    document.getElementById("txtIncorrecta3").value = pregunta.rI3;
+}
 
-    let txtPregunta = pregunta1.pregunta;
-    let txtCorrecta = pregunta1.rCorrecta;
-    let txtIncorrecta1 = pregunta1.rI1;
-    let txtIncorrecta2 = pregunta1.rI2;
-    let txtIncorrecta3 = pregunta1.rI3;
-
-    let arrayTextos = [];
-
-    arrayTextos.push(txtPregunta);
-    arrayTextos.push(txtCorrecta);
-    arrayTextos.push(txtIncorrecta1);
-    arrayTextos.push(txtIncorrecta2);
-    arrayTextos.push(txtIncorrecta3);
-
-    for (let index = 0; index < arrayTextos.length; index++) {
-        arrayTextos[index] = arrayTextos[index].replaceAll("\\", "");
-
-
-    }
-
-    document.getElementById("txtPregunta").value = arrayTextos[0];
-    document.getElementById("txtCorrecta").value = arrayTextos[1];
-    document.getElementById("txtIncorrecta1").value = arrayTextos[2];
-    document.getElementById("txtIncorrecta2").value = arrayTextos[3];
-    document.getElementById("txtIncorrecta3").value = arrayTextos[4];
-
-
-
-};
+function guardarPreguntas() {
+    const jsonString = cuestionario.serializar();
+    localStorage.setItem("cuestionario", jsonString);
+}
 
 function generaArchivo() {
     const cuestionarioString = localStorage.getItem("cuestionario");
 
     if (cuestionarioString) {
-        const cuestionario = JSON.parse(cuestionarioString);
+        const cuestionario = Cuestionario.deserializar(cuestionarioString);
 
         // Crear el contenido del archivo
-        const contenidoArchivo = generarContenidoCuestionario(cuestionario);
+        const contenidoArchivo = cuestionario.generarContenidoArchivo();
 
         // Crear un enlace para descargar el archivo
         const enlaceDescarga = document.createElement('a');
@@ -194,30 +98,12 @@ function generaArchivo() {
 
         enlaceDescarga.href = URL.createObjectURL(archivoBlob);
         enlaceDescarga.download = 'cuestionario.txt';
-        enlaceDescarga.textContent='Enlace descarga';
+        enlaceDescarga.textContent = 'Enlace descarga';
 
         const divEnlace = document.getElementById('divEnlace');
-        divEnlace.innerHTML="";
-        divEnlace.appendChild(enlaceDescarga)
-
+        divEnlace.innerHTML = "";
+        divEnlace.appendChild(enlaceDescarga);
     }
-}
-
-function generarContenidoCuestionario(cuestionario) {
-    let contenido = '';
-
-    cuestionario.preguntas.forEach((pregunta, index) => {
-        contenido += `${index + 1}:\n`;
-        contenido += `${pregunta.pregunta}\n`;
-        contenido += `{\n`;
-        contenido += `=${pregunta.rCorrecta}\n`;
-        contenido += `~%-25%${pregunta.rI1}\n`;
-        contenido += `~%-25%${pregunta.rI2}\n`;
-        contenido += `~%-25%${pregunta.rI3}\n`;
-        contenido += `}\n`;
-    });
-
-    return contenido;
 }
 
 
--- DWEC/Practica Cuestionario/objetos.js
+++ DWEC/Practica Cuestionario/objetos.js
@@ -1,9 +1,5 @@
-"use strict";
-
 class Pregunta {
-
-    constructor(pregunta, rCorrecta, rI1, rI2, rI3){
-
+    constructor(pregunta, rCorrecta, rI1, rI2, rI3) {
         this.pregunta = pregunta;
         this.rCorrecta = rCorrecta;
         this.rI1 = rI1;
@@ -11,14 +7,62 @@ class Pregunta {
         this.rI3 = rI3;
     }
 
-
-
+    toHTMLUl() {
+        return `<ul>
+                    <li>Respuesta Correcta: ${this.rCorrecta}</li>
+                    <li>Respuesta Incorrecta 1: ${this.rI1}</li>
+                    <li>Respuesta Incorrecta 2: ${this.rI2}</li>
+                    <li>Respuesta Incorrecta 3: ${this.rI3}</li>
+                </ul>`;
+    }
 }
 
 class Cuestionario {
-
-    constructor(arrayPreguntas){
-        this.preguntas = arrayPreguntas;
+    constructor() {
+        this.preguntas = [];
     }
 
+    añadirPregunta(pregunta) {
+        this.preguntas.push(pregunta);
+    }
+
+    descartarPregunta(id) {
+        this.preguntas.splice(id, 1);
+    }
+
+    getPregunta(i) {
+        return this.preguntas[i];
+    }
+
+    borrarPreguntas() {
+        this.preguntas = [];
+    }
+
+    serializar() {
+        return JSON.stringify(this.preguntas);
+    }
+
+    generarContenidoArchivo() {
+        let contenido = '';
+        this.preguntas.forEach((pregunta, index) => {
+            contenido += `${index + 1}:\n`;
+            contenido += `${pregunta.pregunta}\n`;
+            contenido += `{\n`;
+            contenido += `=${pregunta.rCorrecta}\n`;
+            contenido += `~%-25%${pregunta.rI1}\n`;
+            contenido += `~%-25%${pregunta.rI2}\n`;
+            contenido += `~%-25%${pregunta.rI3}\n`;
+            contenido += `}\n`;
+        });
+        return contenido;
+    }
+
+    static deserializar(jsonString) {
+        const preguntas = JSON.parse(jsonString);
+        const cuestionario = new Cuestionario();
+        preguntas.forEach((pregunta) => {
+            cuestionario.añadirPregunta(new Pregunta(pregunta.pregunta, pregunta.rCorrecta, pregunta.rI1, pregunta.rI2, pregunta.rI3));
+        });
+        return cuestionario;
+    }
 }
