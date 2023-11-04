0e550552cde9b547c78627b00fd1ee49ed916a67
lucasgil93 <lucasgildominguez@gmail.com>
2023-11-04 21:01:07 +0100

    commit final final este si

--- DIW/Proyecto/code.js
+++ DIW/Proyecto/code.js
@@ -69,3 +69,12 @@ if (isDarkMode === "true") {
   setLightMode();
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
