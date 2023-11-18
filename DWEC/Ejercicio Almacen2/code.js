c2f8cff45c7e1721b0a989b18f197b521287e61f
lucasgil93 <lucasgildominguez@gmail.com>
2023-11-18 20:23:20 +0100

    commitaso

--- DWEC/Ejercicio Almacen2/code.js
+++ DWEC/Ejercicio Almacen2/code.js
@@ -1,2 +1,35 @@
 "use strict";
 
+    var btnMostrarAlta = document.getElementById("btnMostrarAlta");
+    var btnMostrarVenta = document.getElementById("btnMostrarVenta");
+    var btnMostrarTotales = document.getElementById("btnMostrarTotales");
+    var btnMostrarListado = document.getElementById("btnMostrarListado");
+
+    btnMostrarAlta.addEventListener("click", function() {
+        alert("Se hizo clic en el botón Alta de bicicleta");
+
+    });
+
+    btnMostrarVenta.addEventListener("click", function() {
+        alert("Se hizo clic en el botón Venta de bicicleta");
+
+    });
+
+    btnMostrarTotales.addEventListener("click", function() {
+        alert("Se hizo clic en el botón Totales");
+    });
+
+    btnMostrarListado.addEventListener("click", function() {
+        alert("Se hizo clic en el botón Listado separado");
+    });
+
+    var btnVentaBicicleta = document.getElementById("btnVentaBicicleta");
+    var btnAltaBicicleta = document.getElementById("btnAltaBicicleta");
+
+    btnVentaBicicleta.addEventListener("click", function() {
+        alert("Se hizo clic en el botón Venta de bicicleta");
+    });
+
+    btnAltaBicicleta.addEventListener("click", function(){
+        alert("Se hizo click en el boton Alta de Bicicleta");
+    });
