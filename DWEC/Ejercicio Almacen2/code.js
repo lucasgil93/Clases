6105cd09e7e29050473a10eaea3f250125c22651
lucasgil93 <lucasgildominguez@gmail.com>
2023-11-27 20:35:35 +0100

    commit almacen2

--- DWEC/Ejercicio Almacen2/code.js
+++ DWEC/Ejercicio Almacen2/code.js
@@ -1,35 +1,163 @@
-"use strict";
+﻿"use strict";
 
-    var btnMostrarAlta = document.getElementById("btnMostrarAlta");
-    var btnMostrarVenta = document.getElementById("btnMostrarVenta");
-    var btnMostrarTotales = document.getElementById("btnMostrarTotales");
-    var btnMostrarListado = document.getElementById("btnMostrarListado");
 
-    btnMostrarAlta.addEventListener("click", function() {
-        alert("Se hizo clic en el botón Alta de bicicleta");
+////////////////
+// FUNCTIONS
+//////////////////
+const ocultarTodoslosFormularios=()=>{
+  document.querySelectorAll(".form-horizontal").forEach( f=>{
+    f.classList.add("oculto");
+  });
+}
 
-    });
 
-    btnMostrarVenta.addEventListener("click", function() {
-        alert("Se hizo clic en el botón Venta de bicicleta");
 
-    });
 
-    btnMostrarTotales.addEventListener("click", function() {
-        alert("Se hizo clic en el botón Totales");
-    });
+const altaBicicleta=()=>{
+  //leermos los atributos comunes
+  const localizador = document.querySelector("#txtLocalizador").value;
+ 
+  const año = document.querySelector("#lstAnio").value;
 
-    btnMostrarListado.addEventListener("click", function() {
-        alert("Se hizo clic en el botón Listado separado");
-    });
+  const tipo = document.querySelector("[name=rbtTipoBicicleta]:checked").value;
+  
+  
+  //Tipo == "carretera" o "montana"
+  let oBici;
 
-    var btnVentaBicicleta = document.getElementById("btnVentaBicicleta");
-    var btnAltaBicicleta = document.getElementById("btnAltaBicicleta");
+  if(tipo === "carretera"){
+    const numPlatos = document.querySelector("#txtPlatos").value;
+    oBici = new Carretera(localizador, año, false,numPlatos);
+  }
+  else{
+    const numSuspensiones = document.querySelector("#txtSuspensiones").value;
+    oBici = new Montaña(localizador, año, false,numSuspensiones);
+  }
 
-    btnVentaBicicleta.addEventListener("click", function() {
-        alert("Se hizo clic en el botón Venta de bicicleta");
-    });
+  // Añadimos la bicicleta
+  if( tienda.altaBicicleta(oBici) ){
+    alert("Alta bicicleta OK");
+  }
+  else{
+    alert("Bicicleta registrada previamente");
+  }
 
-    btnAltaBicicleta.addEventListener("click", function(){
-        alert("Se hizo click en el boton Alta de Bicicleta");
-    });
+  //ocultamos el formulario
+  ocultarTodoslosFormularios();
+}
+
+
+
+
+
+const ventaBicicleta=()=>{
+  //leermos los atributos comunes
+  const localizador = document.querySelector("#txtLocalizadorVenta").value;
+ 
+  const respuesta = tienda.ventaBici(localizador);
+  alert(respuesta);
+}
+
+
+
+
+const calculoTotales = () =>{
+  const totalBicicletas = tienda.tBicis.length;
+  const totalCarretera = tienda.tBicis.filter( b=>b instanceof Carretera).length;
+  const totalMontaña = tienda.tBicis.filter( b=>b instanceof Montaña).length;
+  const totalVentas = tienda.tBicis.filter( b=>b.vendida).length;
+  const totalNovendidas = tienda.tBicis.filter( b=>!b.vendida).length;
+
+
+  // let contador = 0;
+  // for(let i=0; i<tienda.tBicis.length;i++){
+  //   if( tienda.tBicis[i] instanceof Montaña)
+  //     contador++;
+  // }
+
+
+  const totales = document.querySelector("#totales");
+  totales.innerHTML=`
+  <h3>Bicicletas de carretera: ${totalCarretera} </h3>
+  <h3>Bicicletas de montaña: ${totalMontaña} </h3>
+  <h3>Total de bicicletas: ${totalBicicletas} </h3>
+  <h3>Total de ventas: ${totalVentas} </h3>
+  <h3>Total no vendidas: ${totalNovendidas} </h3>
+  `;
+}
+
+
+
+const mostrarListados = () =>{
+  const totales = document.querySelector("#totales");
+
+  let s=`<table>
+    </tr>
+        <th>localizador</th><th>año</th><th>vendida</th><th>numSuspensiones</th>
+    </tr>`;
+    
+  const montañas = tienda.tBicis.filter( b=>b instanceof Montaña);
+  for(let b of montañas){
+    s+= b.toHTMLrow();
+  }
+
+  s+="</table>";
+
+  s+=`<table>
+  </tr>
+      <th>localizador</th><th>año</th><th>vendida</th><th>numSuspensiones</th>
+  </tr>`;
+  
+  const carreteras = tienda.tBicis.filter( b=>b instanceof Carretera);
+  for(let b of carreteras){
+    s+= b.toHTMLrow();
+  }
+
+  s+="</table>";
+  totales.innerHTML=s;
+
+}
+
+
+
+//////////////////
+// MAIN
+//////////////////
+//1.- Ocultamos todos los formularios
+ocultarTodoslosFormularios();
+
+
+//2.- Mostramos los formularios
+document.querySelector("#btnMostrarAlta").addEventListener("click", e=>{
+  ocultarTodoslosFormularios();
+  document.querySelector("[form=frmAltaBicicleta]").classList.remove("oculto");
+});
+
+document.querySelector("#btnMostrarVenta").addEventListener("click", e=>{
+  ocultarTodoslosFormularios();
+  document.querySelector("[form=frmVentaBicicleta]").classList.remove("oculto");
+});
+
+//3.- Alta de bicicleta
+document.querySelector("#btnAltaBicicleta").addEventListener("click", e=>{
+  altaBicicleta(e);
+});
+
+
+//4.- Venta de bicicleta
+document.querySelector("#btnVentaBicicleta").addEventListener("click", e=>{
+  ventaBicicleta();
+});
+
+//5.- Creamos el objeto Tienda
+let tienda = new Tienda([],0);
+
+//6.- Cálculo de totales
+document.querySelector("#btnMostrarTotales").addEventListener("click", e=>{
+  calculoTotales();
+});
+
+//6.- Mostrar listados
+document.querySelector("#btnMostrarListado").addEventListener("click", e=>{
+  mostrarListados();
+});
