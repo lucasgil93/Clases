18abdc393e1f8d6a1b2bb0dfd82c1268a7d5b7cc
lucasgil93 <lucasgildominguez@gmail.com>
2023-12-05 20:14:10 +0100

    commitaso fin dwec

--- DWEC/Tema 6/Ejercicio 1/code.js
+++ DWEC/Tema 6/Ejercicio 1/code.js
@@ -1,3 +1,8 @@
 "use strict";
 
 let resultado = document.querySelector("#resultado");
+
+
+resultado.firstElementChild.innerHTML="Lista de Animales";
+
+resultado.firstElementChild.nextElementSibling
